16e12f6b2d3719bfb0287fc14da18fe051c58224
Azahramirez <88936883+Azahramirez@users.noreply.github.com>
2025-04-28 18:10:57 -0600

    modelo base mas prueba en carga de archivo numpy fold 0

---
+++ src/model.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+class build:
+    @staticmethod
+    def build_it():
+        model = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D((2, 2)),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(10, activation='softmax')
+        ])
+        model.compile(
+            optimizer='adam',
+            loss='sparse_categorical_crossentropy',
+            metrics=['accuracy']
+        )
+        return model
