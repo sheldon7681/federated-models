2fda4752da2422cb102f2cb0cbbda04ceed447c0
Azahramirez <88936883+Azahramirez@users.noreply.github.com>
2025-05-01 13:18:55 -0600

    crear modelofold0, plantilla para los demás folds

--- src/modelG.py
+++ src/modelG.py
@@ -4,12 +4,12 @@ class build:
     @staticmethod
     def build_it():
         model = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+            tf.keras.Input(shape=(28, 28, 1)),  # Input layer explícita
+            tf.keras.layers.Conv2D(16, (5, 5), activation='relu', input_shape=(28, 28, 1)),
             tf.keras.layers.MaxPooling2D((2, 2)),
-            tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-            tf.keras.layers.MaxPooling2D((2, 2)),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Conv2D(32, (5, 5), activation='relu'),
+            tf.keras.layers.GlobalAveragePooling2D(),
+            tf.keras.layers.Dense(32, activation='relu'),
             tf.keras.layers.Dense(10, activation='softmax')
         ])
         model.compile(
